27999348ab1254f2af5bd8551b23d7369bf0f42b
GregoryAgnew <gregory.agnew@revature.net>
2021-06-01 16:31:16 -0500

    Adding the ability to read name and age and return them via console

---
+++ consoleapp/nameandage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace nameandage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            String yourname = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            String yourage = Console.ReadLine();
+            Console.WriteLine($"Your name is {yourname} and your age is {yourage}.");
+        }
+    }
+}
